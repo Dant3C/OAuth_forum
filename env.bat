e7d6673ebcc3eaa920dc65564b9e7eacef3b2b87
JackGarza04 <97976283+JackGarza04@users.noreply.github.com>
2022-05-05 09:30:26 -0700

    Update env.bat

--- env.bat
+++ env.bat
@@ -1,6 +1,6 @@
 ::remove when done testing (set env vars in Heroku)
-set GITHUB_CLIENT_ID= 
-set GITHUB_CLIENT_SECRET=
+set GITHUB_CLIENT_ID=9f1d7ac6afb26c9b1719
+set GITHUB_CLIENT_SECRET=0f7b94d60f46d9f1b4fbc3a824fd6969afd0017c
 set APP_SECRET_KEY=
 
 set MONGO_CONNECTION_STRING=mongodb+srv://Dant3C:<SET PASSWORD HERE>@dsw-cluster.krow0.mongodb.net/OAuth-forum?retryWrites=true
